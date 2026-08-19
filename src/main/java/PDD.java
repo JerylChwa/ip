ad8715b5668d1d00d0903107a2b4e12b15f0fdf7
Jeryl <jeryl0308@gmail.com>
2026-08-19 17:18:51 +0800

    Add ability to unmark tasks as done
    Adds an 'unmark <index>' command that reverses a task's done status
    back to not-done, mirroring the existing 'mark' command. No new
    classes added, per the requirement.

--- src/main/java/PDD.java
+++ src/main/java/PDD.java
@@ -40,6 +40,11 @@ public class PDD {
                 done.set(index, true);
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + statusIcon(done.get(index)) + " " + tasks.get(index));
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                done.set(index, false);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + statusIcon(done.get(index)) + " " + tasks.get(index));
             } else {
                 tasks.add(input);
                 done.add(false);
